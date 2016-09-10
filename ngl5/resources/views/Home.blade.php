8f3fdc57002f2e10d60d59c68638c4948cb172ad
Apik_BPrasad <Apik_BPrasad>
2016-09-10 12:42:44 +0530

    css and js files added

---
+++ ngl5/resources/views/Home.blade.php
@@ -0,0 +1,4 @@
+@extends('templates.default')
+@section('content')
+  <h1>Welcome</h1>
+@stop
---
+++ ngl5/resources/views/templates/default.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Home::Moviesfyi</title>
+  <link rel="stylesheet" href="{{URL::asset('css/app.css')}}" media="screen" title="no title">
+
+</head>
+<body>
+    <div class="container">
+      @yield('content')
+    </div>
+
+<script src="{{URL::asset('js/angular.min.js')}}" charset="utf-8"></script>
+<script src="{{URL::asset('js/jquery.min.js')}}" charset="utf-8"></script>
+</body>
+</html>
