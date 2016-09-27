e69441275eb2c8e2222051f86fe918840e700cad
prasadbv <prasad1331@gmail.com>
2016-09-27 20:50:09 +0530

    html part updated

--- ngl5/public/js/homeapp.js
+++ ngl5/public/js/homeapp.js
@@ -8,14 +8,20 @@ homeapp.config(function($routeProvider) {
         templateUrl: 'http://localhost/ng-l5/ngl5/public',
         controller: 'indexController'
     });
-
 });
-homeapp.controller('indexController',function($scope,$location,$http){
-$scope.movs = 'test message';
+homeapp.controller('indexController',function($scope,$window,$http){
+$scope.movs = [];
   $scope.init = function(){
-    $http.get('http://localhost/ng-l5/ngl5/public/mov').success(function(data,status,headers,config){
-      $scope.movs ="testmessage";
+    $http.post('http://localhost/ng-l5/ngl5/public/mov').success(function(data,status,headers,config){
+      $scope.movs = data;
     });
   }
   $scope.init();
+  $scope.test = function(mid){
+    if(mid){ }else{  mid =1; }
+    $scope.$apply(function () {
+            $scope.mmid = mid;
+        });
+  }
+
 });
