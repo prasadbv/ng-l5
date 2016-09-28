b24210b0c249b9dfd04e69408827764baaba11ae
thanush <beingthanush@gmail.com>
2016-09-28 16:40:53 +0530

    artist table and relation added

---
+++ ngl5/database/seeds/ArtistSeeder.php
@@ -0,0 +1,28 @@
+<?php
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+
+class ArtistSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        $gen = ['male','female'];
+        foreach(range(1,50) as $index){
+          DB::table('artists')->insert([
+            'artist_name'		=>$faker->name($gender = null|'male'|'female'),
+            'artist_gender'		=>$faker->randomElement($gen),
+            'artist_pic'		=>$faker->imageUrl($width = 100, $height = 100),
+            'artist_description'=>$faker->text($maxNbChars = 200),
+            'created_at' 		=> Carbon::now()->format('Y-m-d H:i:s'),
+            'updated_at' 		=>Carbon::now()->format('Y-m-d H:i:s')
+          ]);
+        };
+    }
+}
--- ngl5/database/seeds/MoviesSeeder.php
+++ ngl5/database/seeds/MoviesSeeder.php
@@ -8,8 +8,9 @@ class MoviesSeeder extends Seeder
     public function run()
     {
         $faker = Faker::create();
-        $lang = ['english','hindi','telugu','tamil','malayalam'];
+        $lang  = ['english','hindi','telugu','tamil','malayalam'];
         $genre = ['Action','Adventure','Animation','Biography','Comedy','Crime','Drama','Family','Fantasy','Horror','Mystery','Romance','Sci-Fi','Thriller'];
+        $artist = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
         foreach(range(1,50) as $index){
           DB::table('movies')->insert([
             'name'=>$faker->name,
@@ -17,6 +18,7 @@ class MoviesSeeder extends Seeder
             'genre'=>$faker->randomElement($genre).",".$faker->randomElement($genre),
             'image'=>$faker->imageUrl($width = 220, $height = 330),
             'certificate'=>'A',
+            'artist_id'=>$faker->randomElement($artist).",".$faker->randomElement($artist),
             'status'=>$faker->randomElement([1,2,3,4]),
             'rating'=>$faker->randomElement([1,2,3,4,5]),
             'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
