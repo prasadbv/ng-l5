90608a4c9a456ad727f7a88d89d780fcf17a2b0b
thanush <beingthanush@gmail.com>
2016-10-17 13:42:11 +0530

    DB for heroku seted

--- ngl5/config/database.php
+++ ngl5/config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-04.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_a3a91dcd34868f0'),
+            'username' => env('DB_USERNAME', 'b374603da2dda9'),
+            'password' => env('DB_PASSWORD', 'cdf05500'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
