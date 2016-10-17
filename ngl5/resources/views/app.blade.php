1c85b20331162723516889f0bd5f55de4e1355ef
thanush <beingthanush@gmail.com>
2016-10-17 12:46:19 +0530

    procfile updated

--- ngl5/resources/views/app.blade.php
+++ ngl5/resources/views/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <base href="http://ng-l5.app/" />
+  <base href="/" />
   <title>Home::Moviesfyi</title>
   <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i" rel="stylesheet">
   <link rel="stylesheet" href="{{URL::asset('css/lib/bootstrap.css')}}">
